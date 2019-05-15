2d9d72fd29e4e2b42be5a86fa99983c18cc17fea
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-15 11:42:23 -0700

    Colors snapshot test passing

--- src/components/Colors.test.js
+++ src/components/Colors.test.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+import Colors from './Colors';
+
+describe('Colors component', () => {
+  it('renders Colors', () => {
+    const colors = [
+      { name: 'Red', rgb: { red: 200, green: 0, blue: 0 } },
+      { name: 'Blue', rgb: { red: 0, green: 0, blue: 255 } }
+    ];
+    const wrapper = shallow(<Colors colors={colors} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
