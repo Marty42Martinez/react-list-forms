a47d809bc585fef47b9627a6457ebf36083e5a2c
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-15 14:14:46 -0700

    PAlbum compnonent styling

--- src/components/photos/PhotoAlbum.js
+++ src/components/photos/PhotoAlbum.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Photos from './Photos';
-//import styles
+import styles from './PhotoAlbum.css';
 
 function PhotoAlbum({ title, photos }) {
   return (
-    <section className='place'>
+    <section className={styles.PhotoAlbum}>
       <h2>{title}</h2>
       <Photos photos={photos} />
     </section>
