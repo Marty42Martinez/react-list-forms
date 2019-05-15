1fda16585d12d8bb9bd233f5d16370d053d8cf7c
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-15 14:06:35 -0700

    PAlbum component sans styling

---
+++ src/components/photos/PhotoAlbum.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Photos from './Photos';
+//import styles
+
+function PhotoAlbum({ title, photos }) {
+  return (
+    <section className='place'>
+      <h2>{title}</h2>
+      <Photos photos={photos} />
+    </section>
+  );
+}
+
+PhotoAlbum.propTypes = {
+  title: PropTypes.string,
+  photos: PropTypes.array.isRequired
+};
+
+PhotoAlbum.defaultProps = {
+  title: 'My Album'
+};
+
+export default PhotoAlbum;
