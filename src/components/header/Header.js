41d1945d9740f94dcdebd428cd2cddd1543d3bf3
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-15 14:21:06 -0700

    header component sans styling

---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import styles
+
+export default function Header() {
+  return (
+    <header className='holder'>
+      <h1>My Cool Photos</h1>
+    </header>
+  );
+}
