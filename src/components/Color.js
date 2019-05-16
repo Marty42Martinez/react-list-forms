77524312b3af74cedd20e692a9d456774ed3bc0d
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-16 11:10:34 -0700

    Color rgb2hex fns

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const decimalFromHex = hex => {
+  return Number.parseInt(`0x${hex}`, 16);
+};
+
+const rgbFromHex = hex => {
+  return {
+    red: decimalFromHex(hex.slice(1, 3)),
+    green: decimalFromHex(hex.slice(3, 5)),
+    blue: decimalFromHex(hex.slice(5, 7))
+  };
+};
+
+
 function Color({ name, hex }) {
   const rgb = rgbFromHex(hex);
 
