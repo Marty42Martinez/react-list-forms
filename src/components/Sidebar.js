8170e122926428a75ab817a2703aed2d7eba8a76
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-15 17:11:22 -0700

    Sidebar styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//import styles
+import styles from './Sidebar.css';
 
 function Sidebar({ children }) {
   return (
-    <nav className="holder">
+    <nav className={styles.Sidebar}>
       {children}
     </nav>
   );
