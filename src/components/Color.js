e13a857a63c61548a5107b96c42c6cb68e92996e
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-16 11:08:23 -0700

    Color Fnc comp, no helper fns

---
+++ src/components/Color.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Color({ name, hex }) {
+  const rgb = rgbFromHex(hex);
+
+  const colorStyle = {
+    display: 'inline-block',
+    backgroundColor: hex,
+    width: '0.8em',
+    height: '0.8em'
+  };
+
+  return(
+    <dl>
+      <dt>Name</dt>
+      <dd>{name || hex} <div style={colorStyle}></div></dd>
+
+      <dt>Hex</dt>
+      <dd>{hex}</dd>
+
+      <dt>RGB</dt>
+      <dd>
+        <p>Red - {rgb.red}</p>
+        <p>Green - {rgb.green}</p>
+        <p>Blue - {rgb.blue}</p>
+      </dd>
+    </dl>
+  );
+}
+
+Color.propTypes - {
+  name: PropTypes.string,
+  hex: PropTypes.string.isRequired
+};
+
+export default Color;
